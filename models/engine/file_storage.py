0564687cc7149e23d77c7733183be247ee65e21e
jeremias <jeremiaserba7894@gmail.com>
2023-08-01 19:09:26 +0000

    Task 5 - Add comment and imports

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 """Defines a class to manage file storage."""
 import json
-import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 
 class FileStorage:
