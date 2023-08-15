bfe7d7a89ed3630949745f11107717c521a3196c
root <root@DESKTOP-OFAJ8QV.localdomain>
2023-08-15 14:20:50 -0300

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
-"""script that starts a Flask web application"""
-
+"""List of states"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states', strict_slash=False)
-def cities_by_states():
-    """comments"""
-    states_dict = storage.all(State)
-    dict = {}
-    for val in states_dict.values():
-        dict[val] = val.cities
-    return render_template('8-cities_by_states.html', dict=dict)
-
-
 @app.teardown_appcontext
-def close(exception):
-    """Close database connection"""
+def app_teardown_appcontext(self):
+    "Close the session after each Request"
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    return render_template("8-cities_by_states.html",
+                           states=storage.all(State).values())
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
