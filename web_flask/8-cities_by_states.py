621090f51d0f1bf6269eb98a659c971250d3fcb2
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-08-17 06:34:55 -0700

    task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,21 +1,26 @@
 #!/usr/bin/python3
-"""List of states"""
+"""Task 8. Script that starts a Flask web application.
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
 def app_teardown_appcontext(self):
-    "Close the session after each Request"
+    """After each request, remove the current SQLAlchemy Session
+    """
     storage.close()
 
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    return render_template("8-cities_by_states.html",
-                           states=storage.all(State).values())
+    """Display a HTML page with the list of cities by states
+    """
+    return render_template('8-cities_by_states.html',
+                           states=storage.all(State))
 
 
 if __name__ == "__main__":
