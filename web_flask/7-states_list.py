7d7ff4a270643c672ed433555b368b55cae1dc79
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-08-17 08:48:47 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-"""Write a script that starts a Flask web application"""
+"""
+This module defines a simple Flask web application.
+"""
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    "Close the session after each request"
+def app_teardown_appcontext(self):
+    """After each request this removes the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    "Write a script that starts a Flask web application"
-    return render_template("7-states_list.html",
-                           states=storage.all(State).values())
+    return render_template('7-states_list.html', states=storage.all(State))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
