9e030283c73b33c2debe01b1455e0907366c3d4d
JoaquinGarcia2408 <joacogarcia2408@gmail.com>
2023-08-17 06:20:08 -0700

    task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""task 8"""
-from flask import Flask, request, jsonify
+"""List of states"""
+from flask import Flask, render_template
 from models import storage
-
+from models.state import State
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def app_teardown_appcontext(self):
     "Close the session after each request"
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
 def states_list():
     return render_template("7-states_list.html",
                            states=storage.all(State).values())
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
